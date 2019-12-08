1f9dd15ffc80c8aa60a9058bfbdaa917c83ae51e
Romańczyk Przemysław <przemyslaw.romanczyk@microsoft.wsei.edu.pl>
2019-12-08 12:33:40 +0100

    Topic 8

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,18 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
     ///Zadanie 0 ////////////////////////////////////////////
-    var klasa = document.getElementsByClassName("title");
-    var getDataAnimation = function(element){
-        var newElement = element;
-        return newElement;
+    const klasa = document.querySelector("div .title");
+    const getDataAnimation = function(element){
+        return element.getAttribute("data-animation");
     }
-    getDataAnimation(klasa);
+    console.log(getDataAnimation(klasa));
 
     ///Zadanie 1 ////////////////////////////////////////////
 
-    var element = document.getElementById("home");
-    var element2 = document.querySelector("home");
+    const element = document.getElementById("home");
+    console.log(element);
+    const element2 = document.querySelector("#home");
+    console.log(element2);
 
-    var element3 = document.querySelector("data-direction");
+    const element3 = document.querySelector("li");
+    const element4 = element3.getAttribute("data-direction")
+    console.log(element4);
 
+    const element5 = document.querySelector(".block");
+    console.log(element5);
+
+    ///Zadanie 2 ////////////////////////////////////////////
+    const element6 = document.querySelectorAll("nav li");
+    console.log(element6);
+    console.log(element6.length);
+    const element7 = document.querySelectorAll("div p");
+    console.log(element7);
+    console.log(element7.length);
+    const element8 = document.querySelectorAll("article div");
+    console.log(element8);
+    console.log(element8.length);
+
+    ///Zadanie 3 ////////////////////////////////////////////
+    const element9 = document.querySelector("article.first");
+    let result = element9.getElementsByTagName("h1")
+    console.log(result.length);
 });
