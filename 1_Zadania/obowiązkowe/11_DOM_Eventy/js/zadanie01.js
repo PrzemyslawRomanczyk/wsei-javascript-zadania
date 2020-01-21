9c980eb71ad94454ce70cf62e0a719e738f80994
PrzemyslawRomanczyk <49125731+PrzemyslawRomanczyk@users.noreply.github.com>
2020-01-21 18:42:23 +0100

    Topic 11
    Answers for 0-3

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -1,5 +1,9 @@
 var menu = document.querySelector("#menu");
 var paragraf = document.querySelector("p");
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+window.addEventListener('DOMContentLoaded', () => {
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+})
+
+// querySelector nie znajdzie elementów w momencie uruchomienia kodu ponieważ nie pojawiły się jeszcze w DOMie
